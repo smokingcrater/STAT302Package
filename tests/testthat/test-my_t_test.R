13b8e30a4657412c98e1d6b4ef3e8f5437210502
Brad Scurlock <smokingcrater@gmail.com>
2021-08-18 18:24:38 -0700

    Added tests to test-my_t_test.R

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testdata <- rnorm(100, mean = 0, sd = 1)
+test_that("my_t.test() non-numeric input for parameter x or mu throws error", {
+  expect_error(my_t.test("one"))
+  expect_error(my_t.test(c("one", "two")))
+  expect_error(my_t.test(rnorm(100, mean = 0, sd = 1), mu = "zero"))
+})
+test_that("my_t.test() invalid input for parameter alternative throws error", {
+  expect_error(my_t.test(rnorm(100, mean = 0, sd = 1), alternative = "unsure"))
+})
+test_that("my_t.test() returns a list", {
+  expect_type(my_t.test(rnorm(100, mean = 0, sd = 1)), "list")
+})
+test_that("my_t.test() returns a list", {
+  expect_type(my_t.test(rnorm(100, mean = 0, sd = 1)), "list")
+})
+test_that("my_t.test() and t.test() return identical values for same inputs", {
+  expect_equal(my_t.test(testdata, alternative = "two.sided", mu = 0)$p_val,
+               t.test(testdata, alternative = "two.sided", mu = 0)$p.value)
 })
