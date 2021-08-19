c941087bdf1ff21887305be38a17abdf90166306
Brad Scurlock <smokingcrater@gmail.com>
2021-08-19 16:56:02 -0700

    Added tests for output when feedback = TRUE in my_knn_cv() and my_rf_cv()

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -36,4 +36,10 @@ test_that("my_knn_cv() returns a list", {
   expect_type(my_knn_cv(train, cl, k_nn = 1, k_cv = 5, feedback = FALSE),
               "list")
 })
-
+test_that("my_knn_cv() no message for parameter feedback throws error", {
+  train <- as.data.frame(rbind(iris3[1:25,,1], iris3[1:25,,2], iris3[1:25,,3]))
+  cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
+  expect_output(my_knn_cv(train, cl, feedback = TRUE))
+  expect_output(my_knn_cv(train, cl, k_nn = 1, feedback = TRUE))
+  expect_output(my_knn_cv(train, cl, k_nn = 1, k_cv = 5, feedback = TRUE))
+})
--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -9,4 +9,7 @@ test_that("my_rf_cv() invalid input for parameter feedback throws error", {
 test_that("my_rf_cv() returns a double", {
   expect_type(my_rf_cv(k = 5, feedback = FALSE), "double")
 })
-
+test_that("my_rf_cv() no message for parameter feedback throws error", {
+  expect_output(my_rf_cv(feedback = TRUE))
+  expect_output(my_rf_cv(k = 5, feedback = TRUE))
+})
