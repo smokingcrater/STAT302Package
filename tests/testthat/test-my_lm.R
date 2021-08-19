6b4609f478a0948e43a552cfc86c617c9c22cf1a
Brad Scurlock <smokingcrater@gmail.com>
2021-08-19 13:44:02 -0700

    Added a test for my_lm()

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -9,3 +9,7 @@ test_that("my_lm() parameter pp < 0 or > 8 throws error", {
   expect_error(my_lm(mpg ~ hp + wt, data = mtcars, pp = -1))
   expect_error(my_lm(mpg ~ hp + wt, data = mtcars, pp = 9))
 })
+test_that("my_lm() and lm() return identical values for same inputs", {
+  expect_equal(as.numeric(my_lm(mpg ~ hp + wt, data = mtcars, pp = 5)[1, 1]),
+               as.numeric(lm(mpg ~ hp + wt, data = mtcars)$coefficients[1]))
+})
